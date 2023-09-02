5f5a1054d7139b09d617951c3cae1d1a7d623b0a
Kyle Givler <xvegan88x@gmail.com>
2023-09-02 12:48:54 -0400

    Chapter 1

---
+++ MyFirstEfCoreApp/MyFirstEfCoreApp/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstEfCoreApp;
+
+public class AppDbContext : DbContext
+{
+    private const string connectionString = 
+        @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyFirstEfCoreApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Trusted_Connection=True";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    public DbSet<Book> Books { get; set; } = default!;
+}
---
+++ MyFirstEfCoreApp/MyFirstEfCoreApp/MyLoggerProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstEfCoreApp;
+public class MyLoggerProvider : ILoggerProvider
+{
+    private readonly List<string> _logs;
+
+    public MyLoggerProvider(List<string> logs)
+    {
+        _logs = logs;
+    }
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new MyLogger(_logs);
+    }
+
+    public void Dispose()
+    {
+    }
+
+    private class MyLogger : ILogger
+    {
+        private readonly List<string> _logs;
+
+        public MyLogger(List<string> logs)
+        {
+            _logs = logs;
+        }
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel >= LogLevel.Information;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            _logs.Add(formatter(state, exception));
+        }
+    }
+}
